9cb62f66642c7d6269d8d0ebf8edf18e35dadb6e
Roman Vildanov <vildanovv.rr@gmail.com>
2022-10-26 22:53:11 +0300

    refactoring hw3 images

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,70 +1,63 @@
 import { Button } from 'components/Button/Button';
 import { Loader } from 'components/Loader/Loader';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchPhotos } from 'services/ImagesAPI';
 import { Gallery } from './ImageGallery.styled';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 import { Notify } from 'notiflix';
 import PropTypes from 'prop-types';
 
-export class ImageGallery extends Component {
-  state = {
-    images: null,
-    page: 2,
-    isLoading: false,
-    totalHits: null,
-  };
+export const ImageGallery = ({ searchQuery }) => {
+  const [images, setImages] = useState(null)
+  const [page, setPage] = useState(2)
+  const [isLoading, setIsLoading] = useState(false)
+  // const [totalHits, setTotalHits] = useState(null)
 
-  componentDidUpdate(prevProps, _) {
-    if (
-      prevProps.searchQuery !== this.props.searchQuery &&
-      this.props.searchQuery !== ''
-    ) {
-      this.setState({ isLoading: true });
-      fetchPhotos(this.props.searchQuery).then(response => {
-        
-        if (response.hits.length === 0) {
-          Notify.failure('Wrong request');
-          this.setState({ isLoading: false });
-        } else {
-          this.setState({ images: [...response.hits], isLoading: false, totalHits: response.totalHits });
-        }
-      });
+
+  useEffect(() => {
+    if(searchQuery === '') {
+      return
     }
-  }
+    setIsLoading(true)
+    fetchPhotos(searchQuery, page).then(response => {
+      if(response.hits.length === 0) {
+        Notify.failure('Wrong request')
+        setIsLoading(false)
+      } else if(page > 1) {
+          setImages(prevImages => [...prevImages, ...response.hits])
+      }
+      setImages(response.hits);        
+      setIsLoading(false)
+        
+      
+    })
+  }, [page, searchQuery])
 
-  loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  useEffect(() => {
+    setPage(1);
+  }, [searchQuery]);
 
-    this.setState({ isLoading: true });
-
-    fetchPhotos(this.props.searchQuery, this.state.page).then(response =>
-      this.setState(prevState => ({
-        images: [...prevState.images, ...response.hits],        
-        isLoading: false,
-      }))
-    );
+  const loadMore = () => {
+    setPage(prevState => prevState.page + 1);
   };
 
-  render() {
-    const { images, isLoading } = this.state;
-    return (
-      <>
-        {isLoading && <Loader />}
-        {images && (
-          <Gallery>
-            {images.map(item => {
-              return <ImageGalleryItem key={item.id} item={item} />;
-            })}
-          </Gallery>
-        )}
-        {images && images.length !== this.state.totalHits && (<Button children={'Load more'} onClick={this.loadMore} />)}
-      </>
-    );
-  }
+  
+    
+  return (
+    <>
+      {isLoading && <Loader />}
+      {images && (
+        <Gallery>
+          {images.map(item => {
+            return <ImageGalleryItem key={item.id} item={item} />;
+          })}
+        </Gallery>
+      )}
+      {images && (<Button children={'Load more'} onClick={loadMore} />)}
+    </>
+  );
 }
+
 
 ImageGallery.propTypes = {
   searchQuery: PropTypes.string,
